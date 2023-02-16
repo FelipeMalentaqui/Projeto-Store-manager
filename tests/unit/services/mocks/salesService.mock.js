123ccaffcbc4c009886559f3dc10cfdd55a058cd
Felipe <felipemalentaq@hotmail.com>
2023-02-16 17:17:09 -0300

    teste da camada service de sales ok

--- tests/unit/services/mocks/salesService.mock.js
+++ tests/unit/services/mocks/salesService.mock.js
@@ -12,6 +12,19 @@ const sales = [{
   }
 ];
 
+const saleId = [{
+  "date": "2021-09-09T04:54:29.000Z",
+  "productId": 1,
+  "quantity": 2
+},
+{
+  "date": "2021-09-09T04:54:54.000Z",
+  "productId": 2,
+  "quantity": 2
+}
+];
+
 module.exports = {
   sales,
+  saleId,
 }
--- tests/unit/services/salesServices.test.js
+++ tests/unit/services/salesServices.test.js
@@ -3,16 +3,45 @@ const { expect } = require('chai');
 
 const { salesService } = require('../../../src/services');
 const { salesModel } = require('../../../src/models');
-const { sales } = require('./mocks/salesService.mock');
+const { sales, saleId } = require('./mocks/salesService.mock');
 
 describe('Testando a Sales Service', function () {
   describe('testando GetAll', function () {
+    afterEach(() => {
+      sinon.restore();
+    });
+
+    
     it('Retorna todas as vendas', async function () {
       sinon.stub(salesModel, 'getAll').resolves(sales);
 
       const result = await salesService.getAll();
 
       expect(result).to.deep.equal(sales);
+    });
+  });
+
+  describe('Testando findById', function () {
+    afterEach(() => {
+      sinon.restore();
+    });
+
+     it('Retorna um erro se passado um id invalido', async function () {
+       sinon.stub(salesModel, 'findById').resolves([]);
+
+       const result = await salesService.findById(10);
+
+       expect(result.type).to.equal('SALE_NOT_FOUND');
+       expect(result.message).to.equal('Sale not found');
+     });
+
+    it('Devera retornar todos os produtos como id selecionado', async function () {
+      sinon.stub(salesModel, 'findById').resolves(saleId);
+
+      const result = await salesService.findById(1);
+
+      expect(result.type).to.equal(null);
+      expect(result.message).to.equal(saleId);
     });
   });
 });
